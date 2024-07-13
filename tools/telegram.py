3f3f63609487ec7b8dbecab3bafb317d0d94219f
Victor Y. Fadeev <victor.y.fadeev@gmail.com>
2024-07-13 15:34:02 +0300

    Simplify

--- tools/telegram.py
+++ tools/telegram.py
@@ -4,6 +4,7 @@ import csv
 import glob
 
 from bs4 import BeautifulSoup, Tag
+from functools import reduce
 
 
 def message_filter(div: Tag) -> bool:
@@ -15,51 +16,21 @@ def message_filter(div: Tag) -> bool:
 	return classes == [['title', 'bold'], ['status', 'details']]
 
 
+def import_from_html(path: str) -> list[tuple[str, str]]:
+	with open(path, encoding='utf-8') as fp:
+		soup = BeautifulSoup(fp, 'html.parser')
+
+		messages = soup.select('.message.default')
+		bodies = map(lambda div: div.select_one('.body'), messages)
+		filtered = filter(message_filter, bodies)
+
+		pairs = map(lambda body: (body.select_one('.date')['title'], body.select_one('.status').string), filtered)
+		return list(pairs)
+
+
 if __name__ == '__main__':
 	root = os.path.dirname(__file__)
 	paths = map(lambda path: os.path.join(root, path), glob.glob('messages*.html', root_dir=root))
 
-	for html in paths:
-		with open(html, encoding='utf-8') as fp:
-			soup = BeautifulSoup(fp)
-			messages = soup.select('.message.default')
-
-			bodies = map(lambda div: div.select_one('.body'), messages)
-			bodies = list(bodies)
-
-			filtered = filter(message_filter, bodies)
-			filtered = list(filtered)
-
-
-			pairs = map(lambda body: (body.select_one('.date')['title'], body.select_one('.status').string), filtered)
-			pairs = list(pairs)
-
-			print(pairs[0])
-
-
-			# classes = lambda body: list(map(lambda div: div['class'], body.find_all()))
-			# filtered = filter(lambda body: map(lambda div: div['class'], body.select_one('.body').find_all()) == [['title', 'bold'], ['status', 'details']], bodies)
-
-
-			# print(len(list(filtered)))
-			# print(len(pairs))
-			# print(classes(pairs[0][1]))
-
-
-
-			# print(messages[0].select_one('.body'))
-			# print([div['class'] for div in messages[0]])
-
-			# print(len(messages[0].find_all(recursive=False)))
-			# print(messages[0].select_one('.pull_right.date.details')['title'])
-			# print(messages[0].select_one('.body').select_one('.pull_right.date.details')['title'])
-
-			# print(len(list(messages[0].contents)))
-			# print(len(list(messages[0].select('div.body')[0].contents)))
-
-
-
-			# # print(len(messages))
-			# message = soup.find('div', class_='message default clearfix')
-			# # print(message.find('div', class_='body').prettify())
-			# print(message.class_)
+	calls = reduce(lambda lhs, rhs: [*lhs, *rhs], map(import_from_html, paths))
+	print(len(calls))
